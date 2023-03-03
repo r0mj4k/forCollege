cafac96469a62ebcdd19df090552d45fe94eb2d8
r0mj4k <126695989+r0mj4k@users.noreply.github.com>
2023-03-03 16:48:37 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    while(true){
+    System.out.println("Podaj liczbe: ");
+    int n = scanner.nextInt();
+
+    boolean isPrimeNumber = true;
+    
+    for(int i=2;i<n;i++) {
+      if(n%i == 0)
+        isPrimeNumber = false;
+    }
+
+    String string = isPrimeNumber ? "Liczba pierwsza" : "Liczba złożona";
+    System.out.println(string);
+    }
+  }
+}
