fcb1cdee8c831579d910e5f844a730b4a117c4d8
r0mj4k <126695989+r0mj4k@users.noreply.github.com>
2023-03-07 18:25:33 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    int[][] a = new int[10][10];
+    for(int i=0;i<10;i++){
+      for(int j=0;j<10;j++){
+        a[i][j] = (i+1)*(j+1);
+        System.out.print(a[i][j] + "\t");
+      }
+      System.out.println();
+    }
+  }
+}
